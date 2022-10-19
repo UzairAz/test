47bf9e93f5f30f9d211af1ea7e51557bf8325f86
UzairAz <98876103+UzairAz@users.noreply.github.com>
2022-10-19 12:15:28 +0500

    Add files via upload

---
+++ product_modification/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Modification",
+    'summary': """Add Extra Fields to Product template and Product Variants""",
+    'description': """
+        Add Extra Fields to Product template and Product Variants
+    """,
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'product'],
+    'data': [
+        'views/views.xml',
+    ]
+}
