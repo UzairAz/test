f41da2334107292f91de404182d2e55865dcc91d
UzairAz <98876103+UzairAz@users.noreply.github.com>
2022-11-07 10:32:18 +0500

    Add files via upload

--- warehouse_group/models/models.py
+++ warehouse_group/models/models.py
@@ -6,10 +6,19 @@ from odoo import models, fields, api
 class InheritStockQuant(models.Model):
     _inherit = 'stock.quant'
 
+    get_warehouse = fields.Many2one("stock.warehouse", compute="_get_warehouse_id")
     warehouse_id = fields.Many2one("stock.warehouse")
+
+    @api.depends('location_id', 'product_id', 'warehouse_id')
+    def _get_warehouse_id(self):
+        self.warehouse_id = self.location_id.warehouse_id.ids[0] if len(
+            self.location_id.warehouse_id.ids) > 1 else self.location_id.warehouse_id.id
+        # self.warehouse_id = self.location_id.warehouse_id.ids[0] if len(
+        #     self.location_id.warehouse_id.ids) > 1 else self.location_id.warehouse_id.id
+        self.get_warehouse = 1
 
     @api.model
     def _get_inventory_fields_create(self):
         res = super(InheritStockQuant, self)._get_inventory_fields_create()
-        res.append('warehouse_id')
+        res.append('warehouse_id', 'get_warehouse')
         return res
